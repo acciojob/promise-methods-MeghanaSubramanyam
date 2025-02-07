cebc211b503fa2b57879eede0ea4d70c6f110622
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2025-02-07 11:48:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
 function myRace(promises) {
-  // implementation
+    return new Promise((resolve, reject) => {
+        for (const promise of promises) {
+            Promise.resolve(promise).then(resolve, reject);
+        }
+    });
 }
 
 function myAny(promises) {
-  // implementation
+    return new Promise((resolve, reject) => {
+        let rejections = 0;
+        for (const promise of promises) {
+            Promise.resolve(promise).then(resolve).catch(() => {
+                rejections++;
+                if (rejections === promises.length) {
+                    reject("all promises rejected");
+                }
+            });
+        }
+    });
 }
 
 function myAll(promises) {
-  // implementation
+    return new Promise((resolve, reject) => {
+        let results = new Array(promises.length);
+        let completed = 0;
+        promises.forEach((promise, index) => {
+            Promise.resolve(promise).then(value => {
+                results[index] = value;
+                completed++;
+                if (completed === promises.length) {
+                    resolve(results);
+                }
+            }).catch(reject);
+        });
+    });
 }
 
 function myAllSettled(promises) {
-  // implementation
+    return new Promise((resolve) => {
+        let results = new Array(promises.length);
+        let completed = 0;
+        promises.forEach((promise, index) => {
+            Promise.resolve(promise).then(value => {
+                results[index] = { status: 'fulfilled', value };
+            }).catch(error => {
+                results[index] = { status: 'rejected', error };
+            }).finally(() => {
+                completed++;
+                if (completed === promises.length) {
+                    resolve(results);
+                }
+            });
+        });
+    });
 }
 
 module.exports = {
